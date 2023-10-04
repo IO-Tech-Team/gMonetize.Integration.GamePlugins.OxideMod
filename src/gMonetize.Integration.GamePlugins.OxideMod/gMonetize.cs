478b2cf3f4314460f9ecc898843d357c925b540c
2chevskii <prsroman3@gmail.com>
2023-10-04 10:42:01 +0300

    Start importing gMonetize v2 into repo

--- src/gMonetize.Integration.GamePlugins.OxideMod/gMonetize.cs
+++ src/gMonetize.Integration.GamePlugins.OxideMod/gMonetize.cs
@@ -1,7 +1,37 @@
-﻿namespace gMonetize.Integration.GamePlugins.OxideMod
+﻿using System.Diagnostics;
+using Oxide.Core;
+using Oxide.Core.Libraries.Covalence;
+
+namespace Oxide.Plugins
 {
-    public class gMonetize
+    [Info("gMonetize", "gMonetize Project", "2.0.0")]
+    public class gMonetize : CovalencePlugin
     {
-        
+        private const string PERMISSION_USE = "gmonetize.use";
+        public const string CMD_OPEN = "gmonetize.open";
+        public const string CMD_CLOSE = "gmonetize.close";
+        public const string CMD_NEXT_PAGE = "gmonetize.nextpage";
+        public const string CMD_PREV_PAGE = "gmonetize.prevpage";
+        public const string CMD_RETRY_LOAD = "gmonetize.retryload";
+        public const string CMD_REDEEM_ITEM = "gmonetize.redeemitem";
+
+        private static gMonetize Instance;
+
+        [Conditional("DEBUG")]
+        private static void LogDebug(string format, params object[] args)
+        {
+            Interface.Oxide.LogDebug("[gMonetize] " + format, args);
+        }
+
+        void Init()
+        {
+            Instance = this;
+        }
+
+        void OnServerInitialized()
+        {
+        }
+
+        private bool CanUsePlugin(IPlayer player) => player.HasPermission(PERMISSION_USE);
     }
 }
